7c4733e58aed4be5b2f3fe25d05a4c3a41a013ce
root <hachemico@gmail.com>
2022-02-21 12:00:03 +0100

    lazy load react

--- Frontend/bicibike/src/App.js
+++ Frontend/bicibike/src/App.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
-import AdminPanel from './pages/adminPanel/adminPanel'
-import Bikes from './pages/adminPanel/bikes'
-import Footer from "./components/Footer/footer"
-import Header from "./components/Header/header"
-import Home  from './pages/home/home'
-import Login from "./pages/login/login";
-import Register  from './pages/register/register'
-import Station  from './pages/station/station'
-import Stations  from './pages/stations/stations'
-
 import './App.css'
 import { StationsContextProvider } from "./context/StationsContext";
 import { UserContextProvider } from "./context/UserContext";
 import { RentsContextProvider } from "./context/RentContext";
 import {BikeContextProvider} from "./context/BikeContext"
+import Spinner from './components/Spinner/spinner'
+
+const AdminPanel = React.lazy(() => import("./pages/adminPanel/adminPanel"))
+const Bikes = React.lazy(() => import("./pages/adminPanel/bikes"))
+const Footer = React.lazy(() => import("./components/Footer/footer"))
+const Header= React.lazy(() => import("./components/Header/header"))
+const Home = React.lazy(() => import("./pages/home/home"))
+const Login = React.lazy(() => import("./pages/login/login"))
+const Register  = React.lazy(() => import("./pages/register/register"))
+const Station = React.lazy(() => import("./pages/station/station"))
+const Stations  = React.lazy(() => import("./pages/stations/stations"))
 
 function App() {
   return (
       <Router>
+        <Suspense fallback={<Spinner />}>
         <UserContextProvider>
         <div className="App">
           <section className="App-content">
@@ -46,6 +47,7 @@ function App() {
           </section>
         </div>
         </UserContextProvider>
+        </Suspense>
       </Router>
   );
 }
